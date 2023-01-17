e8229e0bad2d720fa2f25e54af1e94ed862216c0
dow2 <dowedwards@gmail.com>
2023-01-17 15:58:56 -0600

    deployment

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
-import { BsGithub, BsLinkedin, BsMailbox2} from 'react-icons/bs'
+import React from 'react';
+import { BsGithub, BsLinkedin, BsMailbox2 } from 'react-icons/bs';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
       <div>
-        <BsGithub />
+        <a
+          href='https://github.com/dow2'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsGithub />
+        </a>
       </div>
       <div>
-        <BsLinkedin />
+        <a
+          href='https://www.linkedin.com/in/dow-edwards2/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <BsMailbox2 />
+        <a
+          href='mailto:dowedwards@dow2tech.com'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsMailbox2 />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
